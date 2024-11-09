ef8a70f4cef127f3a10ec643f8a20419a2449728
meshach567 <arinzemeshach123@gmail.com>
2024-11-09 14:12:39 +0100

    work on the serializers

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers  import DefaultRouter
+from core.views import ProductViewSet, OrderViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1', include('djoser.urls')),
-    path('api/v1', include('djoser.urls.authtoken'))
+    path('api/', include(router.urls)),
+    path('auth', include('djoser.urls')),
+    path('auth', include('djoser.urls.jwt'))
 ]
